536380b4fb7b0c5e3d49b6d784757a97b505130f
Fedor Kaputsky <fkaptusky@mail.ru>
2021-08-01 21:49:16 +0300

    initial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# monsters
+# Monsters Robolex
+
+Basic app with the use of create-react-app
---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: []
+    };
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <CardList>
+          {this.state.monsters.map(monster =>
+            <h1 key={monster.id}>{monster.name}</h1>
+          )}
+        </CardList>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(resp => resp.json())
+      .then(resp => this.setState({ monsters: resp }));
+  }
+}
+
+export default App;

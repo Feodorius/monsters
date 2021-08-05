e9679564c5b2df2b09efd283ee7b8a117c208437
Fedor Kaputsky <fedor_kaputsky@epam.com>
2021-08-05 22:44:51 +0300

    feat: add monsters project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import './App.css';
 import React, { Component } from 'react';
+import { SearchBox } from './components/search-box/search-box.component';
 import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
-  render() {
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
 
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField));
     return (
       <div className="App">
-        <CardList>
-          {this.state.monsters.map(monster =>
-            <h1 key={monster.id}>{monster.name}</h1>
-          )}
-        </CardList>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={this.handleChange} />
+        <CardList
+          monsters={filteredMonsters} />
       </div>
     );
   }
